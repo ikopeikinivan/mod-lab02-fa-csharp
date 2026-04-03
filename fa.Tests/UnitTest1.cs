de7cf08d4ae03cbf472989b96c327d72d4c5c282
ikopeikinivan <ikopeikin@icloud.com>
2026-04-03 22:54:39 +0300

    b8 Копейкин Иван

--- fa.Tests/UnitTest1.cs
+++ fa.Tests/UnitTest1.cs
@@ -93,166 +93,167 @@ namespace NET
             FA3 fa = new FA3();
             bool? result = fa.Run(s);
             Assert.IsTrue(result == false);
-        }     
+        }
+
+        [TestMethod]
+        public void FA1_Valid_OneZero_OneOne()
+        {
+            FA1 fa = new FA1();
+            Assert.IsTrue(fa.Run("01"));
+        }
+
+        [TestMethod]
+        public void FA1_Valid_OneZero_ManyOnes()
+        {
+            FA1 fa = new FA1();
+            Assert.IsTrue(fa.Run("0111"));
+        }
+
+        [TestMethod]
+        public void FA1_Valid_OneZero_OneOne_TrailingZeros()
+        {
+            FA1 fa = new FA1();
+            Assert.IsTrue(fa.Run("10"));
+        }
+
+        [TestMethod]
+        public void FA1_Valid_OneZero_Complex()
+        {
+            FA1 fa = new FA1();
+            Assert.IsTrue(fa.Run("1010111"));
+        }
+
+        [TestMethod]
+        public void FA1_Invalid_NoZero()
+        {
+            FA1 fa = new FA1();
+            Assert.IsFalse(fa.Run("111"));
+        }
+
+        [TestMethod]
+        public void FA1_Invalid_TwoZeros_NoOne()
+        {
+            FA1 fa = new FA1();
+            Assert.IsFalse(fa.Run("00"));
+        }
+
+        [TestMethod]
+        public void FA1_Invalid_TwoZeros_WithOnes()
+        {
+            FA1 fa = new FA1();
+            Assert.IsFalse(fa.Run("010"));
+        }
+
+        [TestMethod]
+        public void FA1_Invalid_OnlyZero()
+        {
+            FA1 fa = new FA1();
+            Assert.IsFalse(fa.Run("0"));
+        }
+
+        [TestMethod]
+        public void FA2_Valid_OneZero_OneOne()
+        {
+            FA2 fa = new FA2();
+            Assert.IsTrue(fa.Run("01"));
+        }
+
+        [TestMethod]
+        public void FA2_Valid_ThreeZeros_OneOne()
+        {
+            FA2 fa = new FA2();
+            Assert.IsTrue(fa.Run("0001"));
+        }
+
+        [TestMethod]
+        public void FA2_Valid_OneZero_ThreeOnes()
+        {
+            FA2 fa = new FA2();
+            Assert.IsTrue(fa.Run("0111"));
+        }
+
+        [TestMethod]
+        public void FA2_Valid_Mixed()
+        {
+            FA2 fa = new FA2();
+            Assert.IsTrue(fa.Run("001011"));
+        }
+
+        [TestMethod]
+        public void FA2_Invalid_EvenZeros_EvenOnes()
+        {
+            FA2 fa = new FA2();
+            Assert.IsFalse(fa.Run("0011"));
+        }
+
+        [TestMethod]
+        public void FA2_Valid_OddZeros_OddOnes()
+        {
+            FA2 fa = new FA2();
+            Assert.IsTrue(fa.Run("0010"));
+        }
+
+        [TestMethod]
+        public void FA2_Invalid_EvenZeros_OddOnes()
+        {
+            FA2 fa = new FA2();
+            Assert.IsFalse(fa.Run("001"));
+        }
+
+        [TestMethod]
+        public void FA2_Invalid_Empty()
+        {
+            FA2 fa = new FA2();
+            Assert.IsFalse(fa.Run(""));
+        }
+
+        [TestMethod]
+        public void FA3_Valid_Simple11()
+        {
+            FA3 fa = new FA3();
+            Assert.IsTrue(fa.Run("11"));
+        }
+
+        [TestMethod]
+        public void FA3_Valid_WithZeros()
+        {
+            FA3 fa = new FA3();
+            Assert.IsTrue(fa.Run("0110"));
+        }
+
+        [TestMethod]
+        public void FA3_Valid_Multiple11()
+        {
+            FA3 fa = new FA3();
+            Assert.IsTrue(fa.Run("11011"));
+        }
+
+        [TestMethod]
+        public void FA3_Valid_EndsWith11()
+        {
+            FA3 fa = new FA3();
+            Assert.IsTrue(fa.Run("1011"));
+        }
+
+        [TestMethod]
+        public void FA3_Invalid_No11()
+        {
+            FA3 fa = new FA3();
+            Assert.IsFalse(fa.Run("10101"));
+        }
+
+        [TestMethod]
+        public void FA3_Invalid_OnlyOne1()
+        {
+            FA3 fa = new FA3();
+            Assert.IsFalse(fa.Run("1"));
+        }
+
+        [TestMethod]
+        public void FA3_Invalid_OnlyZeros()
+        {
+            FA3 fa = new FA3();
+            Assert.IsFalse(fa.Run("000"));
+        }
     }
-    [TestMethod]
-public void FA1_Valid_OneZero_OneOne()
-{
-    FA1 fa = new FA1();
-    Assert.IsTrue(fa.Run("01"));
-}
-
-[TestMethod]
-public void FA1_Valid_OneZero_ManyOnes()
-{
-    FA1 fa = new FA1();
-    Assert.IsTrue(fa.Run("0111"));
-}
-
-[TestMethod]
-public void FA1_Valid_OneZero_OneOne_TrailingZeros()
-{
-    FA1 fa = new FA1();
-    Assert.IsTrue(fa.Run("10"));
-}
-
-[TestMethod]
-public void FA1_Valid_OneZero_Complex()
-{
-    FA1 fa = new FA1();
-    Assert.IsTrue(fa.Run("1010111"));
-}
-
-[TestMethod]
-public void FA1_Invalid_NoZero()
-{
-    FA1 fa = new FA1();
-    Assert.IsFalse(fa.Run("111"));
-}
-
-[TestMethod]
-public void FA1_Invalid_TwoZeros_NoOne()
-{
-    FA1 fa = new FA1();
-    Assert.IsFalse(fa.Run("00"));
-}
-
-[TestMethod]
-public void FA1_Invalid_TwoZeros_WithOnes()
-{
-    FA1 fa = new FA1();
-    Assert.IsFalse(fa.Run("010"));
-}
-
-[TestMethod]
-public void FA1_Invalid_OnlyZero()
-{
-    FA1 fa = new FA1();
-    Assert.IsFalse(fa.Run("0"));
-}
-
-[TestMethod]
-public void FA2_Valid_OneZero_OneOne()
-{
-    FA2 fa = new FA2();
-    Assert.IsTrue(fa.Run("01"));
-}
-
-[TestMethod]
-public void FA2_Valid_ThreeZeros_OneOne()
-{
-    FA2 fa = new FA2();
-    Assert.IsTrue(fa.Run("0001"));
-}
-
-[TestMethod]
-public void FA2_Valid_OneZero_ThreeOnes()
-{
-    FA2 fa = new FA2();
-    Assert.IsTrue(fa.Run("0111"));
-}
-
-[TestMethod]
-public void FA2_Valid_Mixed()
-{
-    FA2 fa = new FA2();
-    Assert.IsTrue(fa.Run("001011"));
-}
-
-[TestMethod]
-public void FA2_Invalid_EvenZeros_EvenOnes()
-{
-    FA2 fa = new FA2();
-    Assert.IsFalse(fa.Run("0011"));
-}
-
-[TestMethod]
-public void FA2_Valid_OddZeros_OddOnes()
-{
-    FA2 fa = new FA2();
-    Assert.IsTrue(fa.Run("0010"));
-}
-
-[TestMethod]
-public void FA2_Invalid_EvenZeros_OddOnes()
-{
-    FA2 fa = new FA2();
-    Assert.IsFalse(fa.Run("001"));
-}
-
-[TestMethod]
-public void FA2_Invalid_Empty()
-{
-    FA2 fa = new FA2();
-    Assert.IsFalse(fa.Run(""));
-}
-
-[TestMethod]
-public void FA3_Valid_Simple11()
-{
-    FA3 fa = new FA3();
-    Assert.IsTrue(fa.Run("11"));
-}
-
-[TestMethod]
-public void FA3_Valid_WithZeros()
-{
-    FA3 fa = new FA3();
-    Assert.IsTrue(fa.Run("0110"));
-}
-
-[TestMethod]
-public void FA3_Valid_Multiple11()
-{
-    FA3 fa = new FA3();
-    Assert.IsTrue(fa.Run("11011"));
-}
-
-[TestMethod]
-public void FA3_Valid_EndsWith11()
-{
-    FA3 fa = new FA3();
-    Assert.IsTrue(fa.Run("1011"));
-}
-
-[TestMethod]
-public void FA3_Invalid_No11()
-{
-    FA3 fa = new FA3();
-    Assert.IsFalse(fa.Run("10101"));
-}
-
-[TestMethod]
-public void FA3_Invalid_OnlyOne1()
-{
-    FA3 fa = new FA3();
-    Assert.IsFalse(fa.Run("1"));
-}
-
-[TestMethod]
-public void FA3_Invalid_OnlyZeros()
-{
-    FA3 fa = new FA3();
-    Assert.IsFalse(fa.Run("000"));
-}
 }
