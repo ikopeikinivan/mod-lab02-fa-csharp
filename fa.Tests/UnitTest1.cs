829954336646bbfd6e0efe656b30a7fa26ce8c83
ikopeikinivan <ikopeikin@icloud.com>
2026-04-03 23:24:23 +0300

    b8 Копейкин Иван

--- fa.Tests/UnitTest1.cs
+++ fa.Tests/UnitTest1.cs
@@ -1,6 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using fans;
+
 namespace NET
 {
     [TestClass]
@@ -9,91 +10,103 @@ namespace NET
         [TestMethod]
         public void TestMethod1()
         {
-            String s = "0111";
+            string s = "0111";
             FA1 fa = new FA1();
-            bool? result = fa.Run(s);
-            Assert.IsTrue(result == true);
+            bool result = fa.Run(s);
+            Assert.IsTrue(result);
         }
+
         [TestMethod]
         public void TestMethod2()
         {
-            String s = "01011";
+            string s = "01011";
             FA1 fa = new FA1();
-            bool? result = fa.Run(s);
-            Assert.IsTrue(result == false);
-        }   
+            bool result = fa.Run(s);
+            Assert.IsFalse(result);
+        }
+
         [TestMethod]
         public void TestMethod3()
         {
-            String s = "110101011";
+            string s = "110101011";
             FA1 fa = new FA1();
-            bool? result = fa.Run(s);
-            Assert.IsTrue(result == false);
-        }  
+            bool result = fa.Run(s);
+            Assert.IsFalse(result);
+        }
+
         [TestMethod]
         public void TestMethod4()
         {
-            String s = "1110111";
+            string s = "1110111";
             FA1 fa = new FA1();
-            bool? result = fa.Run(s);
-            Assert.IsTrue(result == true);
-        }  
+            bool result = fa.Run(s);
+            Assert.IsTrue(result);
+        }
+
         [TestMethod]
         public void TestMethod5()
         {
-            String s = "10";
+            string s = "10";
             FA1 fa = new FA1();
-            bool? result = fa.Run(s);
-            Assert.IsTrue(result == true);
-        } 
+            bool result = fa.Run(s);
+            Assert.IsTrue(result);
+        }
+
         [TestMethod]
         public void TestMethod6()
         {
-            String s = "0101";
+            string s = "0101";
             FA2 fa = new FA2();
-            bool? result = fa.Run(s);
-            Assert.IsTrue(result == false);
-        }  
+            bool result = fa.Run(s);
+            Assert.IsFalse(result);
+        }
+
         [TestMethod]
         public void TestMethod7()
         {
-            String s = "00110011";
+            string s = "00110011";
             FA2 fa = new FA2();
-            bool? result = fa.Run(s);
-            Assert.IsTrue(result == false);
-        }  
+            bool result = fa.Run(s);
+            Assert.IsFalse(result);
+        }
+
         [TestMethod]
         public void TestMethod8()
         {
-            String s = "0001";
+            string s = "0001";
             FA2 fa = new FA2();
-            bool? result = fa.Run(s);
-            Assert.IsTrue(result == true);
-        }  
+            bool result = fa.Run(s);
+            Assert.IsTrue(result);
+        }
+
         [TestMethod]
         public void TestMethod9()
         {
-            String s = "111000";
+            string s = "111000";
             FA2 fa = new FA2();
-            bool? result = fa.Run(s);
-            Assert.IsTrue(result == true);
-        } 
+            bool result = fa.Run(s);
+            Assert.IsTrue(result);
+        }
+
         [TestMethod]
         public void TestMethod10()
         {
-            String s = "00110011";
+            string s = "00110011";
             FA3 fa = new FA3();
-            bool? result = fa.Run(s);
-            Assert.IsTrue(result == true);
+            bool result = fa.Run(s);
+            Assert.IsTrue(result);
         }
+
         [TestMethod]
         public void TestMethod11()
         {
-            String s = "0101";
+            string s = "0101";
             FA3 fa = new FA3();
-            bool? result = fa.Run(s);
-            Assert.IsTrue(result == false);
+            bool result = fa.Run(s);
+            Assert.IsFalse(result);
         }
+
+        // ===== Дополнительные тесты =====
 
         [TestMethod]
         public void FA1_Valid_OneZero_OneOne()
--- fa/program.cs
+++ fa/program.cs
@@ -6,136 +6,146 @@ using System.Threading.Tasks;
 
 namespace fans
 {
-  public class State
-  {
-    public string Name;
-    public Dictionary<char, State> Transitions;
-    public bool IsAcceptState;
-  }
-
-
-  public class FA1
-{
-    private State start;
-    private State foundZero;
-    private State accept;
-    private State reject;
-    private State currentState;
-
-    public FA1()
+    public class State
     {
-        start = new State { Name = "start", IsAcceptState = false, Transitions = new Dictionary<char, State>() };
-        foundZero = new State { Name = "foundZero", IsAcceptState = false, Transitions = new Dictionary<char, State>() };
-        accept = new State { Name = "accept", IsAcceptState = true, Transitions = new Dictionary<char, State>() };
-        reject = new State { Name = "reject", IsAcceptState = false, Transitions = new Dictionary<char, State>() };
-
-        start.Transitions['0'] = foundZero;
-        start.Transitions['1'] = start;
-
-        foundZero.Transitions['0'] = reject;
-        foundZero.Transitions['1'] = accept;
-
-        accept.Transitions['0'] = reject;
-        accept.Transitions['1'] = accept;
-
-        reject.Transitions['0'] = reject;
-        reject.Transitions['1'] = reject;
-
-        currentState = start;
+        public string Name;
+        public Dictionary<char, State> Transitions;
+        public bool IsAcceptState;
     }
 
-    public bool? Run(IEnumerable<char> input)
+    public class FA1
     {
-        State current = currentState;
-        foreach (char symbol in input)
+        private State start;
+        private State foundZero;
+        private State accept;
+        private State reject;
+        private State currentState;
+
+        public FA1()
         {
-            if (!current.Transitions.TryGetValue(symbol, out State next))
-                return null;
-            current = next;
+            start = new State { Name = "start", IsAcceptState = false, Transitions = new Dictionary<char, State>() };
+            foundZero = new State { Name = "foundZero", IsAcceptState = false, Transitions = new Dictionary<char, State>() };
+            accept = new State { Name = "accept", IsAcceptState = true, Transitions = new Dictionary<char, State>() };
+            reject = new State { Name = "reject", IsAcceptState = false, Transitions = new Dictionary<char, State>() };
+
+            start.Transitions['0'] = foundZero;
+            start.Transitions['1'] = start;
+
+            foundZero.Transitions['0'] = reject;
+            foundZero.Transitions['1'] = accept;
+
+            accept.Transitions['0'] = reject;
+            accept.Transitions['1'] = accept;
+
+            reject.Transitions['0'] = reject;
+            reject.Transitions['1'] = reject;
+
+            currentState = start;
         }
-        return current.IsAcceptState;
-    }
-}
 
-public class FA2
-{
-    private State q00;
-    private State q01;
-    private State q10;
-    private State q11;
-    private State active;
-
-    public FA2()
-    {
-        q00 = new State { Name = "q00", IsAcceptState = false, Transitions = new Dictionary<char, State>() };
-        q01 = new State { Name = "q01", IsAcceptState = false, Transitions = new Dictionary<char, State>() };
-        q10 = new State { Name = "q10", IsAcceptState = false, Transitions = new Dictionary<char, State>() };
-        q11 = new State { Name = "q11", IsAcceptState = true, Transitions = new Dictionary<char, State>() };
-
-        q00.Transitions['0'] = q10;
-        q00.Transitions['1'] = q01;
-
-        q01.Transitions['0'] = q11;
-        q01.Transitions['1'] = q00;
-
-        q10.Transitions['0'] = q00;
-        q10.Transitions['1'] = q11;
-
-        q11.Transitions['0'] = q01;
-        q11.Transitions['1'] = q10;
-
-        active = q00;
-    }
-
-    public bool? Run(IEnumerable<char> input)
-    {
-        State current = active;
-        foreach (char symbol in input)
+        public bool? Run(IEnumerable<char> input)
         {
-            if (!current.Transitions.ContainsKey(symbol))
-                return null;
-            current = current.Transitions[symbol];
+            currentState = start;
+
+            State current = currentState;
+            foreach (char symbol in input)
+            {
+                if (!current.Transitions.TryGetValue(symbol, out State next))
+                    return null;
+
+                current = next;
+            }
+
+            return current.IsAcceptState;
         }
-        return current.IsAcceptState;
-    }
-}
-
-public class FA3
-{
-    private State A;
-    private State B;
-    private State C;
-    private State current;
-
-    public FA3()
-    {
-        A = new State { Name = "A", IsAcceptState = false, Transitions = new Dictionary<char, State>() };
-        B = new State { Name = "B", IsAcceptState = false, Transitions = new Dictionary<char, State>() };
-        C = new State { Name = "C", IsAcceptState = true, Transitions = new Dictionary<char, State>() };
-
-        A.Transitions['0'] = A;
-        A.Transitions['1'] = B;
-
-        B.Transitions['0'] = A;
-        B.Transitions['1'] = C;
-
-        C.Transitions['0'] = C;
-        C.Transitions['1'] = C;
-
-        current = A;
     }
 
-    public bool? Run(IEnumerable<char> input)
+    public class FA2
     {
-        State activeState = current;
-        foreach (char ch in input)
+        private State q00;
+        private State q01;
+        private State q10;
+        private State q11;
+        private State active;
+
+        public FA2()
         {
-            if (activeState.Transitions.TryGetValue(ch, out State nextState))
-                activeState = nextState;
-            else
-                return null;
+            q00 = new State { Name = "q00", IsAcceptState = false, Transitions = new Dictionary<char, State>() };
+            q01 = new State { Name = "q01", IsAcceptState = false, Transitions = new Dictionary<char, State>() };
+            q10 = new State { Name = "q10", IsAcceptState = false, Transitions = new Dictionary<char, State>() };
+            q11 = new State { Name = "q11", IsAcceptState = true, Transitions = new Dictionary<char, State>() };
+
+            q00.Transitions['0'] = q10;
+            q00.Transitions['1'] = q01;
+
+            q01.Transitions['0'] = q11;
+            q01.Transitions['1'] = q00;
+
+            q10.Transitions['0'] = q00;
+            q10.Transitions['1'] = q11;
+
+            q11.Transitions['0'] = q01;
+            q11.Transitions['1'] = q10;
+
+            active = q00;
         }
-        return activeState.IsAcceptState;
+
+        public bool? Run(IEnumerable<char> input)
+        {
+            active = q00;
+
+            State current = active;
+            foreach (char symbol in input)
+            {
+                if (!current.Transitions.ContainsKey(symbol))
+                    return null;
+
+                current = current.Transitions[symbol];
+            }
+
+            return current.IsAcceptState;
+        }
     }
-}
+
+    public class FA3
+    {
+        private State A;
+        private State B;
+        private State C;
+        private State current;
+
+        public FA3()
+        {
+            A = new State { Name = "A", IsAcceptState = false, Transitions = new Dictionary<char, State>() };
+            B = new State { Name = "B", IsAcceptState = false, Transitions = new Dictionary<char, State>() };
+            C = new State { Name = "C", IsAcceptState = true, Transitions = new Dictionary<char, State>() };
+
+            A.Transitions['0'] = A;
+            A.Transitions['1'] = B;
+
+            B.Transitions['0'] = A;
+            B.Transitions['1'] = C;
+
+            C.Transitions['0'] = C;
+            C.Transitions['1'] = C;
+
+            current = A;
+        }
+
+        public bool? Run(IEnumerable<char> input)
+        {
+            current = A;
+
+            State activeState = current;
+            foreach (char ch in input)
+            {
+                if (activeState.Transitions.TryGetValue(ch, out State nextState))
+                    activeState = nextState;
+                else
+                    return null;
+            }
+
+            return activeState.IsAcceptState;
+        }
+    }
 }
